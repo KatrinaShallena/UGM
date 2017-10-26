be159834c8eba9dc8368a92e2403f6754980ac4d
Kateryna Chumak <katrina.shallena@gmail.com>
2017-10-26 15:56:25 +0300

    init commit

---
+++ js/header-menu.js
@@ -0,0 +1,19 @@
+(function($){
+	  var ico = $('<i class="fa fa-caret-right"></i>');
+	  $('nav#menu li:has(ul) > a').append(ico);
+	  
+	  $('nav#menu li:has(ul)').on('click', function(){
+	    $(this).toggleClass('open');
+	  });
+	  
+	  $('a#toggle').on('click',function(e){
+	    $('html').toggleClass('open-menu');
+	    return false;
+	  });
+	  
+	  
+	  $('div#overlay').on('click',function(){
+	    $('html').removeClass('open-menu');
+	  })
+	  
+})(jQuery)
